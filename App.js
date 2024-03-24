e1b15548ef129f57b9d52adff701ca39708fe87a
dedarob <euclidesroberto16@gmail.com>
2024-03-24 19:35:37 -0300

    Rodapé e rotas

--- App.js
+++ App.js
@@ -1,76 +1,9 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput,} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-
+import { NavigationContainer } from "@react-navigation/native";
+import {Routes} from './src/routes'
 export default function App() {
-  const [text, setText] = useState('');
-  const [selectedActivity, setSelectedActivity] = useState();
-  const onChangeText = (value) => {
-    setText(value);
-  };
-
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("./src/assets/logo-doctrack.png")}
-        style={styles.logo}
-      />
-
-      <Image
-        source={require("./src/assets/QRCode.png")}
-        style={styles.logo}
-      />
-
-     <Picker
-        style={styles.pickerComponent}
-        selectedValue={selectedActivity}
-        onValueChange={(itemValue, itemIndex) =>
-        setSelectedActivity(itemValue)
-        }>
-        <Picker.Item label="Selecione a especialidade" value="" enabled={false} style={{ color: 'gray' }} />
-        <Picker.Item label="Clínica Médica" value="clinica medica" />
-        <Picker.Item label="Cirurgia" value="cirurgia" />
-        <Picker.Item label="Pediatria" value="pediatria" />
-        <Picker.Item label="Ginecologia e Obstetrícia" value="ginecologia e obstetricia" />
-        <Picker.Item label="Psiquiatria" value="psiquiatria" />
-        <Picker.Item label="Medicina de Família e Comunidade" value="medicina de familia e comunidade" />
-        </Picker>
-
-        <TextInput
-    style={{ height: 200, width: 200, ...styles.textinput, fontWeight: 'normal' }}
-    placeholder='Atividade'
-    onChangeText={onChangeText}
-    value={text}
-    multiline={true}
-    numberOfLines={10}
-/>
-    </View>
+    <NavigationContainer>
+      <Routes/>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F3F3FF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 60,
-  },
-  textinput: {
-    width: '80%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingLeft: 10,
-    marginTop: 20,
-    color: 'black',
-    fontWeight: 'bold'
-  },
-  pickerComponent: {
-    width: 350,
-  },
-});
+  };
---
+++ src/pages/faces/index.js
@@ -0,0 +1,9 @@
+import {View} from 'react-native'
+
+export function Faces(){
+    return(
+        <View>
+            
+        </View>
+    )
+}
